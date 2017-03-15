da8737ccebd2fcaac2977aa4904abdb4e4576cad
David <daurrutia@users.noreply.github.com>
2017-03-15 11:22:24 -0400

    Gets basic details for all domain computers.
    Requires AD module.

---
+++ get-allADcomputers-basic.ps1
@@ -0,0 +1,9 @@
+﻿#GET ALL ADCOMPUTERS 
+#Retrieves basic info (DNSHostName, Enabled, IP, OS, CanonicalName, Created)
+#Author: David U.
+
+Write-Debug "Show what's behind those ellipses. Show me all the objects."
+$FormatEnumerationLimit = -1
+
+Get-ADComputer -Filter * -Properties DNSHostName,Enabled,IPv4Address,OperatingSystem,CanonicalName,whenCreated |
+    Select DNSHostName,Enabled,IPv4Address,OperatingSystem,CanonicalName,whenCreated | Sort DNSHostName | FT -AutoSize
