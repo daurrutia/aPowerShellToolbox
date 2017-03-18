102bff61b0e26c50968ec8e2ba62d5bb91165856
David <daurrutia@users.noreply.github.com>
2017-03-18 16:45:26 -0400

    Install Active Directory on Windows Server
    Tested on Server 2016

---
+++ Install-AD2016.ps1
@@ -0,0 +1,19 @@
+﻿#Install-ADDSForest
+#Windows Server 2016
+
+Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "corp.company.com" `
+-DomainNetbiosName "CORP" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
